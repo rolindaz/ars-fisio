088212c6a751def7bbd003a53896d46e1cc5d4ce
rolindaz <rolinda.zamboni@gmail.com>
2026-04-15 17:56:21 +0200

    feat(HexCard): enhance hexagonal card design with background image and glass effect

--- src/components/HexCard.jsx
+++ src/components/HexCard.jsx
@@ -3,21 +3,22 @@ const HexCard = ({ title, image }) => {
     <div className="hex-container">
       
       {/* Hex shape */}
-      <div className="hex-shape">
-
-        {/* Immagine servizio */}
-        <img
-          src={image}
-          alt={title}
-          className="w-full h-full object-cover"
-        />
+      <div 
+      className="hex-shape" 
+      style={{backgroundImage: `url(${image})`}}>
 
         {/* Overlay */}
         <div className="hex-overlay">
+        </div>
+
+        {/* Glass layer */}
+        <div className="glass" />
+        <div className="hex-content">
           <span className="hex-title">
             {title}
           </span>
         </div>
+
       </div>
 
     </div>
