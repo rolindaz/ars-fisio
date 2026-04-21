c2c3e266a4c7249cb304df128f2ca7364ee51ef6
rolindaz <rolinda.zamboni@gmail.com>
2026-04-21 18:38:44 +0200

    refactor(Servizi): enhance ServiceCard with animation and update layout structure

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,26 +1,24 @@
-export default function ServiceCard({ title, description, image, align }) {
+export default function ServiceCard({ title, description, image, align, delay }) {
   return (
     <>
-      <div
-        className={`
-        flex items-center gap-4 p-4 rounded-xl
-        bg-white/30 backdrop-blur-md border border-white/40
-        shadow-md transition hover:shadow-lg
-        ${align === "right" ? "flex-row-reverse text-right" : ""}
-      `}
-      >
-        {/* Image */}
-        <div className="w-20 h-20 rounded-lg bg-cover bg-center shrink-0"
-        style={{ backgroundImage: `url(${image})` }}/>
-
+      <div style={{ animationDelay: `${delay}ms` }} className={`${align === "right" ? "animate-slide-right" : "animate-slide-left"}`}>
+      <div className={`inner-card group flex justify-between items-center gap-4 rounded-xl bg-white/30 backdrop-blur-md shadow-md transition-all min-h-40 duration-500 hover:shadow-lg hover:translate-y-1 ${align === "right" ? "flex-row-reverse text-right" : ""}`}>
         {/* Text */}
-        <div>
+        <div className="p-3">
           <h3 className={`text-base font-heading font-semibold text-gray-800 mb-1 ${align === "right" ? "text-right" : "text-left"}`}>
             {title}
           </h3>
           <p className="text-sm text-gray-600">{description}</p>
         </div>
+
+        {/* Image */}
+        <div
+          className={`w-28 min-h-32 self-stretch bg-cover bg-center shrink-0 ${align === "right" ? "rounded-l-lg" : "rounded-r-lg"}`}
+          style={{ backgroundImage: `url(${image})` }}
+        />
+        </div>
       </div>
+
     </>
   );
 }
--- src/pages/Servizi.jsx
+++ src/pages/Servizi.jsx
@@ -3,44 +3,44 @@ import ServiceCard from "../components/ServiceCard";
 import services from "../../assets/services/services";
 
 export default function Servizi() {
-
   const left = services.filter((_, i) => i % 2 === 0);
   const right = services.filter((_, i) => i % 2 !== 0);
 
   return (
     <>
-    <Section>
-      <h1 className="text-3xl md:text-4xl font-heading font-bold text-center mb-12">
-        I nostri servizi
-      </h1>
+      <Section>
+        <h1 className="text-3xl md:text-4xl font-heading font-bold text-center mb-12">
+          I nostri servizi
+        </h1>
 
-      <div className="relative grid grid-cols-1 md:grid-cols-2 gap-40 max-w-6xl mx-auto"  style={{ backgroundImage: `url("/logo-fisio-no-text.svg")`, backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundSize: "cover" }}>
+        <div className="relative">
+          <div
+            className="pointer-events-none absolute inset-0 flex justify-center opacity-60"
+            style={{
+              backgroundImage: `url("/logo-fisio-no-text.svg")`,
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+              backgroundSize: "contain",
+            }}
+          >
+          </div>
+            <div className="overlay-services relative grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-40 max-w-5xl mx-auto px-4">
+              {/* LEFT COLUMN */}
+              <div className="flex flex-col gap-6">
+                {left.map((s, i) => (
+                  <ServiceCard key={i} {...s} align="right" delay={i*100} />
+                ))}
+              </div>
 
-        {/* LEFT COLUMN */}
-        <div className="flex flex-col gap-6">
-          {left.map((s, i) => (
-            <ServiceCard key={i} {...s} align="right" />
-          ))}
+              {/* RIGHT COLUMN */}
+              <div className="flex flex-col gap-6">
+                {right.map((s, i) => (
+                  <ServiceCard key={i} {...s} align="left" delay={i*100} />
+                ))}
+              </div>
+            </div>
         </div>
-
-        {/* SPINE */}
-        {/* <div className="hidden md:flex justify-center relative">
-          <img
-            src="/logo-fisio-no-text.svg"
-            alt=""
-            className="h-full max-h-[900px] object-contain opacity-20"
-          />
-        </div> */}
-
-        {/* RIGHT COLUMN */}
-        <div className="flex flex-col gap-6">
-          {right.map((s, i) => (
-            <ServiceCard key={i} {...s} align="left" />
-          ))}
-        </div>
-
-      </div>
-    </Section>
+      </Section>
     </>
-  )
+  );
 }
