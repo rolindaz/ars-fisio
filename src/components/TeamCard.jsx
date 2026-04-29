6b479d53e43866f99f0761d2a12177df8a57b201
rolindaz <rolinda.zamboni@gmail.com>
2026-04-29 11:26:17 +0200

    feat: enhance TeamCard styling with background image and improved layout for the back section

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -16,28 +16,35 @@ export default function TeamCard({
     <>
       <div className="group perspective mx-auto w-fit">
         <div className="flex flex-col items-center">
-          <div className={`team-card-inner corners transform-style flex items-end justify-center overflow-visible bg-transparent
+          <div
+            className={`team-card-inner corners transform-style flex items-end justify-center overflow-visible bg-transparent
           h-[430px] w-[310px] shadow-xl mb-4 sm:h-[447px] sm:w-[332px] md:h-[477px] md:w-[362px] md:shadow-2xl md:mb-6
-        `}>
+        `}
+          >
             <div className="team-card-inner-front corners backface-hidden">
               <div className="team-card-inner-front-bg corners" />
 
-              <img className="relative z-10 object-contain max-h-full px-6" src={image} alt="" />
+              <img
+                className="relative z-10 object-contain max-h-full px-6"
+                src={image}
+                alt=""
+              />
             </div>
 
-            <div className="team-card-back absolute corners-inverted inset-0 backface-hidden rotate-y-180 flex flex-col justify-center px-5">
-              <div className="team-card-quote absolute top-0 left-0 font-quote">
-                “
-              </div>
-              <div className="team-card-description text-base">
-                {descriptionParagraphs.map((paragraph, index) => (
-                  <p
-                    key={`${name}-paragraph-${index}`}
-                    className={index < descriptionParagraphs.length - 1 ? "mb-3" : ""}
-                  >
-                    {paragraph}
-                  </p>
-                ))}
+            <div className="team-card-back absolute corners-inverted inset-0 backface-hidden rotate-y-180">
+              <div className="team-card-back-bg px-5 flex flex-col justify-center">
+                <div className="team-card-description text-base">
+                  {descriptionParagraphs.map((paragraph, index) => (
+                    <p
+                      key={`${name}-paragraph-${index}`}
+                      className={
+                        index < descriptionParagraphs.length - 1 ? "mb-3" : ""
+                      }
+                    >
+                      {paragraph}
+                    </p>
+                  ))}
+                </div>
               </div>
 
               {socials && (
@@ -55,9 +62,7 @@ export default function TeamCard({
             <h3 className="team-card-name font-heading text-lg text-center">
               {name}
             </h3>
-            <h4 className="team-card-caption-role text-center">
-              {caption}
-            </h4>
+            <h4 className="team-card-caption-role text-center">{caption}</h4>
           </div>
         </div>
       </div>
