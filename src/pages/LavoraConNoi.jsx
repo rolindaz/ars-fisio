003cdcc6dde59dc0667957d04870d551e4b30c70
rolindaz <rolinda.zamboni@gmail.com>
2026-04-29 17:29:58 +0200

    feat: simplify form attributes for Netlify integration; comment out hidden input for form name

--- src/pages/LavoraConNoi.jsx
+++ src/pages/LavoraConNoi.jsx
@@ -36,13 +36,13 @@ export default function LavoraConNoi() {
           <form
             name="lavora-con-noi"
             method="POST"
-            data-netlify="true"
+            netlify
             encType="multipart/form-data"
             onSubmit={() => setSubmitted(true)}
             className="form-wwu shadow-md space-y-6"
           >
             {/* Netlify hidden input */}
-            <input type="hidden" name="form-name" value="lavora-con-noi" />
+            {/* <input type="hidden" name="form-name" value="lavora-con-noi" /> */}
 
             {/* Nome e Cognome */}
             <div className="w-full">
