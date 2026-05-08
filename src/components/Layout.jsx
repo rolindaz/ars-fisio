44f252cc6ff2022c60701df51a3831c349e46568
rolindaz <rolinda.zamboni@gmail.com>
2026-05-08 13:12:15 +0200

    feat: Fix bugs in navbar and hero media

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -14,6 +14,7 @@ const Layout = () => {
       </a>
       <div className="flex min-h-screen flex-col">
         <Navbar />
+        <div aria-hidden="true" className="shrink-0" style={{ height: "var(--header-height)" }} />
 
         <main id="main-content" className="flex-1" tabIndex={-1}>
           <Outlet />
